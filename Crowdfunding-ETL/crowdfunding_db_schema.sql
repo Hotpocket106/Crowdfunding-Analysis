be69fcb431e8554a1ca28cb4ff2974777f52bc0a
Jack <112922957+Hotpocket106@users.noreply.github.com>
2022-11-03 15:57:35 -0500

    Add files via upload

---
+++ Crowdfunding-ETL/crowdfunding_db_schema.sql
@@ -0,0 +1,80 @@
+CREATE TABLE "backers"(
+	"backer_id" VARCHAR NOT NULL,
+	"cf_id" INT NOT NULL,
+	"email" VARCHAR NOT NULL,
+	"first_name" VARCHAR NOT NULL,
+	"last_name" VARCHAR NOT NULL
+
+);
+
+SELECT * FROM backers
+
+CREATE TABLE "campaign" (
+    "cf_id" int   NOT NULL,
+    "contact_id" int   NOT NULL,
+    "company_name" varchar(100)   NOT NULL,
+    "description" text   NOT NULL,
+    "goal" numeric(10,2)   NOT NULL,
+    "pledged" numeric(10,2)   NOT NULL,
+    "outcome" varchar(50)   NOT NULL,
+    "backers_count" int   NOT NULL,
+    "country" varchar(10)   NOT NULL,
+    "currency" varchar(10)   NOT NULL,
+    "launch_date" date   NOT NULL,
+    "end_date" date   NOT NULL,
+    "category_id" varchar(10)   NOT NULL,
+    "subcategory_id" varchar(10)   NOT NULL,
+    CONSTRAINT "pk_campaign" PRIMARY KEY (
+        "cf_id"
+     )
+);
+
+CREATE TABLE "category" (
+    "category_id" varchar(10)   NOT NULL,
+    "category_name" varchar(50)   NOT NULL,
+    CONSTRAINT "pk_category" PRIMARY KEY (
+        "category_id"
+     )
+);
+
+CREATE TABLE "subcategory" (
+    "subcategory_id" varchar(10)   NOT NULL,
+    "subcategory_name" varchar(50)   NOT NULL,
+    CONSTRAINT "pk_subcategory" PRIMARY KEY (
+        "subcategory_id"
+     )
+);
+
+CREATE TABLE "contacts" (
+    "contact_id" int   NOT NULL,
+    "first_name" varchar(50)   NOT NULL,
+    "last_name" varchar(50)   NOT NULL,
+    "email" varchar(100)   NOT NULL,
+    CONSTRAINT "pk_contacts" PRIMARY KEY (
+        "contact_id"
+     )
+);
+
+
+ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_contact_id" FOREIGN KEY("contact_id")
+REFERENCES "contacts" ("contact_id");
+
+ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_category_id" FOREIGN KEY("category_id")
+REFERENCES "category" ("category_id");
+
+ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_subcategory_id" FOREIGN KEY("subcategory_id")
+REFERENCES "subcategory" ("subcategory_id");
+
+SELECT * FROM campaign
+
+SELECT backers_count, cf_id FROM campaign
+WHERE (outcome = 'live')
+ORDER BY cf_id DESC
+
+SELECT * FROM backers
+
+SELECT cf_id,
+COUNT (backer_id) as backer_count
+FROM backers
+GROUP BY cf_id
+ORDER BY cf_id DESC;
